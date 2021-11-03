b87e265add05e1074d20be65e8400559e7ea3bbf
BOBAMANLY <jacoblfisher36@gmail.com>
2021-11-03 09:16:57 -0600

    Uploaded format from dice game

---
+++ dice/game/thrower.py
@@ -0,0 +1,26 @@
+import random
+
+# TODO: Define the Thrower class here.
+
+class Thrower:
+    def __init__(self):
+        self.dice = []
+        self.num_throws = 0
+
+    def can_throw(self):
+        if (self.dice.count(5) > 0 or self.dice.count(1) > 0 or self.num_throws == 0):
+            return True
+        else:
+            return False
+
+    def get_points(self):
+        fives = self.dice.count(5) * 50
+        ones = self.dice.count(1) * 100
+        return fives + ones
+
+    def throw_dice(self):
+        self.dice.clear()
+        for _ in range(0, 5):
+            num = random.randint(1,6)
+            self.dice.append(num)
+        self.num_throws += 1
